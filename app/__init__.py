779cfe4345374042b879854878366799b5f29a95
Артур <asimonan97@gmail.com>
2021-05-06 19:34:55 +0300

    Application base configuration

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,14 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 from config import Config
 
 
 db = SQLAlchemy()
 migrate = Migrate()
+login = LoginManager()
 
 
 def create_app(config_class=Config):
@@ -17,6 +19,11 @@ def create_app(config_class=Config):
 
     db.init_app(app)
     migrate.init_app(app)
+    login.init_app(app)
 
-    
+    from app.auth import bp as auth_bp
+
+    app.register_blueprint(auth_bp)
+
+
 
