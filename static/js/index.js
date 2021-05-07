9fcde489c8ed7ad0e4154a3297284eb6daa5c100
Ruslan <sabbath9993@gmail.com>
2021-05-07 19:50:03 +0300

    Modified modal window for login button

--- static/js/index.js
+++ static/js/index.js
@@ -39,6 +39,9 @@ logClose.addEventListener('click', () => {
 content.addEventListener('click', e => {
   e.stopPropagation()
 })
+logContent.addEventListener('click', (e) => {
+  e.stopPropagation()
+})
 
 popupBody.addEventListener('click', () => {
   popup.classList.remove('open')
@@ -54,4 +57,8 @@ logPopupBody.addEventListener('click', () => {
 //   popup.classList.toggle('open')
 //   content.classList.toggle('content__open')
 // })
+
+
+
+// JOIN JS
 
