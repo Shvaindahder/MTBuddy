1db969015345df33522cd93e175b5c19a35658cd
Артур <asimonan97@gmail.com>
2021-05-07 18:21:52 +0300

    bugs fixed

--- app/__init__.py
+++ app/__init__.py
@@ -11,10 +11,11 @@ from config import Config
 db = SQLAlchemy()
 migrate = Migrate()
 login = LoginManager()
+login.login_view = "auth.auth_page"
 
 
 def create_app(config_class=Config):
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder=config_class.TEMPLATES_FOLDER, static_folder=config_class.STATICFILES_FOLDER)
     app.config.from_object(config_class)
 
     db.init_app(app)
@@ -22,8 +23,12 @@ def create_app(config_class=Config):
     login.init_app(app)
 
     from app.auth import bp as auth_bp
+    from app.main import bp as main_bp
 
     app.register_blueprint(auth_bp)
+    app.register_blueprint(main_bp)
+
+    return app
 
 
 
