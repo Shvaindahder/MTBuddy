1d8d2a85baf04f7336db931082c20ab9e65f6890
Артур <asimonan97@gmail.com>
2021-05-07 18:22:18 +0300

    -

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ load_dotenv(os.path.join(BASEDIR, ".env"))
 class Config:
     DEBUG = os.environ.get("DEBUG") or True
     SECRET_KEY = os.environ.get("SECRET_KEY") or "9805d06e7a953055f70448c07f89a7b27a32ab224c3d9c284bd403a1ccb7a7b0"
-    TEMPLATES_FOLDER = os.environ.get(BASEDIR, "templates") 
-    STATICFILES_FOLDER = os.environ.get(BASEDIR, "static")
-    MEDIAFILES_FOLDER = os.environ.get(BASEDIR, "media")
+    TEMPLATES_FOLDER = os.path.join(BASEDIR, "templates") 
+    STATICFILES_FOLDER = os.path.join(BASEDIR, "static")
+    MEDIAFILES_FOLDER = os.path.join(BASEDIR, "media")
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI") or f"sqlite:///{os.path.join(BASEDIR, 'app.db')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
